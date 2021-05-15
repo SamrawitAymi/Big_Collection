f85a196c1d7efbfa65167d923fc55ff239b94a95
Samrawit Haylemeskel <samrieb1980@gmail.com>
2021-05-15 15:49:35 +0200

    add login and registration model, controller and view in Big_collection

---
+++ Big_Collection/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Big_Collection.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Big_Collection.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult LoginPage()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RegisterPage()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult RegisterPage(UserRegistrationViewModel userRegister)
+        {
+            if (ModelState.IsValid)
+            {
+              
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginPage(UserLoginViewModel userLogin)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ Big_Collection/ViewModels/UserRegistrationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Big_Collection.ViewModels
+{
+    public class UserRegistrationViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string RePassword { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
